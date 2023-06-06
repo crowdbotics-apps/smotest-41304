2b48386b40653a4c386756ace9425dfa5fed1d62
crowdbotics-apps <team@crowdbotics.com>
2023-06-06 18:21:56 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Froyt
+admin.site.register(Froyt)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Froyt(models.Model):
+    'Generated Model'
+    cswe = models.BigIntegerField()
